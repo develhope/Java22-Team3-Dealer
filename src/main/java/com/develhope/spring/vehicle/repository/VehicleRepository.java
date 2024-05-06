c93c244e9e194509eedd6e6401e3b956f68df131
Jamming <j.lucreziaparrillo@gmail.com>
2024-05-06 19:23:45 +0200

    updated

--- src/main/java/com/develhope/spring/vehicle/repository/VehicleRepository.java
+++ src/main/java/com/develhope/spring/vehicle/repository/VehicleRepository.java
@@ -2,11 +2,7 @@ package com.develhope.spring.Vehicle.repository;
 
 import com.develhope.spring.Vehicle.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
