4cef56b7b079bdc6511c60955f19504b63f3c02b
Jamming <j.lucreziaparrillo@gmail.com>
2024-05-22 11:19:33 +0200

    TASK: LinkRentUserVehicleModel

--- src/main/java/com/develhope/spring/features/rent/repositories/RentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.features.rent.repositories;
-
-import com.develhope.spring.features.rent.entities.RentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentRepository extends JpaRepository<RentEntity, Long> {
-}
