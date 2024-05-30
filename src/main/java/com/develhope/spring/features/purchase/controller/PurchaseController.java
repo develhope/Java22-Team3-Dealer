62a6d6e23d2ce02ccf1df7bc0d1d29f0255778ec
martacaponnetto <martacaponnetto@yahoo.it>
2024-05-30 17:10:11 +0200

    update Purchase

--- src/main/java/com/develhope/spring/features/purchase/controller/PurchaseController.java
+++ src/main/java/com/develhope/spring/features/purchase/controller/PurchaseController.java
@@ -4,7 +4,6 @@ import com.develhope.spring.features.errors.GenericErrors;
 import com.develhope.spring.features.purchase.DTO.PurchaseRequestDTO;
 import com.develhope.spring.features.purchase.DTO.PurchaseResponseDTO;
 import com.develhope.spring.features.purchase.service.PurchaseService;
-import com.develhope.spring.features.rent.DTOs.RentalResponseDTO;
 import com.develhope.spring.features.user.entity.UserEntity;
 import com.develhope.spring.features.user.model.UserModel;
 import io.vavr.control.Either;
@@ -17,54 +16,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/purchase")
+@RequestMapping("v2/purchases")
 public class PurchaseController {
-@Autowired
-PurchaseService purchaseService;
 
-    @PostMapping("/create/{vehicleId}")
-    public ResponseEntity<?> create(@AuthenticationPrincipal UserEntity userEntity, @RequestBody PurchaseRequestDTO request, @PathVariable Long vehicleId) {
-        Either<GenericErrors, PurchaseResponseDTO> purchase = purchaseService.createPurchase(UserModel.entityToModel(userEntity), request,vehicleId);
-        return purchase.fold(
-                error -> new ResponseEntity<>(new GenericErrors(error.getCode(), "Error creating new rental"), HttpStatus.valueOf(error.getCode())),
-                createPurchase -> new ResponseEntity<>(createPurchase, HttpStatus.CREATED)
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createPurchase(@AuthenticationPrincipal UserEntity userEntity, @RequestBody PurchaseRequestDTO request) {
+        Either<GenericErrors, PurchaseResponseDTO> result = purchaseService.createPurchase(UserModel.entityToModel(userEntity), request);
+        return result.fold(
+                error -> new ResponseEntity<>(new GenericErrors(error.getCode(), "Error creating new purchase"), HttpStatus.valueOf(error.getCode())),
+                created -> new ResponseEntity<>(created, HttpStatus.CREATED)
         );
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@AuthenticationPrincipal UserEntity userEntity, @PathVariable Long id) {
-        Either<GenericErrors, Boolean> result = purchaseService.deletePurchaseById(UserModel.entityToModel(userEntity), id);
+    @DeleteMapping("/delete/{purchaseId}")
+    public ResponseEntity<?> deletePurchase(@AuthenticationPrincipal UserEntity userEntity, @PathVariable Long purchaseId) {
+        Either<GenericErrors, Boolean> result = purchaseService.deletePurchaseById(UserModel.entityToModel(userEntity), purchaseId);
         return result.fold(
                 error -> new ResponseEntity<>(new GenericErrors(error.getCode(), error.getMessage()), HttpStatus.valueOf(error.getCode())),
-                delete -> new ResponseEntity<>(delete, HttpStatus.OK)
+                deleted -> new ResponseEntity<>(deleted, HttpStatus.OK)
         );
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> updatePurchaseById(@AuthenticationPrincipal UserEntity userEntity, @PathVariable Long id, @RequestBody PurchaseRequestDTO request){
-        Either<GenericErrors, PurchaseResponseDTO> upPurchase = purchaseService.updateLinkPurchaseById(UserModel.entityToModel(userEntity), id, request);
-        return upPurchase.fold(
+    @PutMapping("/update/{purchaseId}")
+    public ResponseEntity<?> updatePurchase(@AuthenticationPrincipal UserEntity userEntity, @PathVariable Long purchaseId, @RequestBody PurchaseRequestDTO request) {
+        Either<GenericErrors, PurchaseResponseDTO> result = purchaseService.updatePurchaseById(UserModel.entityToModel(userEntity), purchaseId, request);
+        return result.fold(
                 error -> new ResponseEntity<>(new GenericErrors(error.getCode(), error.getMessage()), HttpStatus.valueOf(error.getCode())),
-                update -> new ResponseEntity<>(update, HttpStatus.OK)
+                updated -> new ResponseEntity<>(updated, HttpStatus.OK)
         );
     }
 
-    @GetMapping("/get/{Id}")
-    public ResponseEntity<?> getSingleById(@AuthenticationPrincipal UserEntity userEntity,@PathVariable Long Id){
-        Either<GenericErrors, PurchaseResponseDTO> purchaseResponseDTO = purchaseService.getSinglePurchase(UserModel.entityToModel(userEntity), Id);
-        return purchaseResponseDTO.fold(
+    @GetMapping("/getSingle/{purchaseId}")
+    public ResponseEntity<?> getSinglePurchase(@AuthenticationPrincipal UserEntity userEntity, @PathVariable Long purchaseId) {
+        Either<GenericErrors, PurchaseResponseDTO> result = purchaseService.getPurchaseById(UserModel.entityToModel(userEntity), purchaseId);
+        return result.fold(
                 error -> new ResponseEntity<>(new GenericErrors(error.getCode(), error.getMessage()), HttpStatus.valueOf(error.getCode())),
                 single -> new ResponseEntity<>(single, HttpStatus.OK)
         );
     }
 
-    @GetMapping("/get/all")
-    public ResponseEntity<?> getAll(@AuthenticationPrincipal UserEntity user){
-        Either<GenericErrors, List<PurchaseResponseDTO>> result = purchaseService.getAllByUserRole(UserModel.entityToModel(user));
-        if(result.isEmpty()){
-            return ResponseEntity.status(422).body("Your purchase list is empty");
-        }
-        return ResponseEntity.ok(result);
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllPurchases(@AuthenticationPrincipal UserEntity userEntity) {
+        Either<GenericErrors, List<PurchaseResponseDTO>> result = purchaseService.getAllPurchases(UserModel.entityToModel(userEntity));
+        return result.fold(
+                error -> new ResponseEntity<>(new GenericErrors(error.getCode(), error.getMessage()), HttpStatus.valueOf(error.getCode())),
+                purchases -> new ResponseEntity<>(purchases, HttpStatus.OK)
+        );
     }
-
 }
